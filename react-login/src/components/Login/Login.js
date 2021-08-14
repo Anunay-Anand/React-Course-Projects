6fa6b75493704c30a392576150cbea098f83d804
Anunay-Anand <anunayanand@gmail.com>
2021-08-14 22:34:59 +0530

    Updated and added useEffect Hook as well as useContext

--- react-login/src/components/Login/Login.js
+++ react-login/src/components/Login/Login.js
@@ -1,8 +1,10 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import Input from "../UI/Input/Input";
+import AuthContext from "../../store/auth-content";
 
 //Reducer funtion
 const emailReducer = (state, action) => {
@@ -82,42 +84,35 @@ const Login = (props) => {
     dispatchPassword({ type: "PASSWORD_BLUR" });
   };
 
+  // Passing email and password on Authentication Context Component for Login
+  const ctx = useContext(AuthContext);
+
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    ctx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${
-            emailState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input
+          id="email"
+          label="E-Mail"
+          type="email"
+          isValid={emailIsValid}
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
+        <Input
+          id="email"
+          label="Password"
+          type="password"
+          isValid={passwordIsValid}
+          value={passwordState.value}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
+        />
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
--- react-login/src/store/auth-content.js
+++ react-login/src/store/auth-content.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // Creating context to manage app wide state
 // The context created here and the object passed is to simply create a default
 const AuthContext = React.createContext({
   isLoggedIn: false,
   onLogout: () => {},
+  onLogin: (email, password) => {},
 });
 
+// We create a context component instead of simply passing values we pass entire component having methods
 export const AuthContextProvider = (props) => {
-  return <AuthContext.Provider>{props.children}</AuthContext.Provider>;
+  // Managing loggin state with useState Hook
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  //Using useEffect to seperate the code from the general flow
+  // useEffect hook is used when we only want to run the code if dependencies change
+  useEffect(() => {
+    //Fetch isloggedIn from local storage
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  // Logout and Login Handler
+  const loginHandler = () => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    //Removing localStorage key of loggedIn
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
