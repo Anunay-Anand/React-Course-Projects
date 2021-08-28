85ed3bea805083e9dff3b4479df9c16cd951d2b7
Anunay-Anand <anunayanand@gmail.com>
2021-08-28 20:09:07 +0530

    used axios to fetch api

--- react-and-database/src/App.js
+++ react-and-database/src/App.js
@@ -1,31 +1,42 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  // State to manage the api data
+  const [movies, setMovies] = useState([]);
+  // State to manage data loaded or not from API
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Making get request with axios
+  const fetchMoviesHandler = async () => {
+    // set Loading State to true
+    setIsLoading(true);
+    const res = await axios.get("https://swapi.dev/api/films/");
+    // Mapping all incoming data
+    // All the data will be stored in transformed array as an array of objects
+    const transformedMovies = res.data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+
+    // Updating the Movies State
+    setMovies(transformedMovies);
+  };
 
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
