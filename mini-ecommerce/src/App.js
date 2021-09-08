980d2f3659c1fd8906e3c5e9c6eff4d717971f98
Anunay-Anand <anunayanand@gmail.com>
2021-09-08 21:38:41 +0530

    added firebase to send put request for cart

--- mini-ecommerce/src/App.js
+++ mini-ecommerce/src/App.js
@@ -1,5 +1,9 @@
+// Importing hooks from React
+import { useEffect } from "react";
+
 // import hooks from redux toolkit and react-redux
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -7,11 +11,26 @@ import Products from "./components/Shop/Products";
 
 function App() {
   // Using useSelector to import the specific state property or state from the state slice object
-  const isCartVisible = useSelector((state) => state.ui.isCartVisible);
+  const showCart = useSelector((state) => state.ui.isCartVisible);
+  // Using useSelector to check if there is a change in cart
+  const cart = useSelector((state) => state.cart);
+
+  // Check if there is a change in cart item added or removed
+  useEffect(() => {
+    // Sending an asynchornous Request to the server
+    const itemToServer = async () => {
+      await axios.put(
+        "https://task-8e5d7-default-rtdb.firebaseio.com/cart.json",
+        JSON.stringify(cart)
+      );
+    };
+    // Run the async function
+    itemToServer();
+  }, [cart]);
 
   return (
     <Layout>
-      {isCartVisible && <Cart />}
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
