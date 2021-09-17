e3505cab68e2953c8f7896e6dcc6787dccfe16a4
Anunay-Anand <anunayanand@gmail.com>
2021-09-18 02:22:22 +0530

    creating our own global hook

--- contextandHooks/src/hooks-store/store.js
+++ contextandHooks/src/hooks-store/store.js
@@ -1,14 +1,26 @@
 // Importing React and other libraries
 import { useState, useEffect } from "react";
 
-let globalStore = {};
+let globalState = {};
 let listeners = [];
 let actions = {};
 
 // Creating Custom Hook
-const useStore = () => {
+export const useStore = () => {
   // We are only interested in the set value of global state
-  const setState = useState(globalStore)[1];
+  const setState = useState(globalState)[1];
+
+  // Creating a global dispatch
+  const dispatch = (actionIdentifier) => {
+    // We pass an actionKey to select specific action from object and provide prevState
+    const newState = actions[actionIdentifier](globalState);
+    // New globalState is then formed with globalState + newState
+    globalState = { ...globalState, ...newState };
+    // Telling all pages of changed state
+    for (let listener of listeners) {
+      listener(globalState);
+    }
+  };
 
   // Creating our global listener
   // Listener is created with simply setState when the component Mounts
@@ -21,6 +33,13 @@ const useStore = () => {
       listeners = listeners.filter((li) => li !== setState);
     };
   }, [setState]);
+
+  return [globalState, dispatch];
 };
 
-export default useStore;
+export const initStore = (userActions, initialState) => {
+  if (!initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
+};
